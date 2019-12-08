8476704796f9f1be909605f7eac2f8726b771cc9
darroyolpz <darroyolpz@users.noreply.github.com>
2019-12-08 23:13:26 +0100

    Looping for fan selection using Pandas

--- web-service-fan-selection.py
+++ web-service-fan-selection.py
@@ -1,4 +1,5 @@
 import json, requests
+import pandas as pd
 
 url = "http://fanselect.net:8079/FSWebService"
 user_ws, pass_ws = 'xxx', 'xxx'
@@ -13,6 +14,10 @@ def get_response(dict_request):
 	url_result = json.loads(url_response.text)
 	return url_result
 
+def sort_function(lst, n):
+	lst.sort(key = lambda x: x[n])
+	return lst 
+
 # Get SessionID
 session_dict = {
 	'cmd': 'create_session',
@@ -22,48 +27,70 @@ session_dict = {
 
 session_id = get_response(session_dict)['SESSIONID']
 print('Session ID:', session_id)
+print('\n')
+
+# Pandas import
+# Open the quotation file
+excel_file = 'EC_FANS.xlsx'
+df = pd.read_excel(excel_file, dtype={'Item': str, 'Gross price': float})
+
+print(df.head())
+print('\n')
+
+inner_list, outter_list = [], []
 
 # Fan parameters to test
-article_no = '115683/A01'
 qv, psf= 28218, 752
-voltage, nominal_frequency = 230, 60
-
-# article_no = '115510/A01'
-# qv, psf= 2500, 200
-# voltage, nominal_frequency = 400, 50
-
 height, width = 1614, 2784
+article_no = '115683/A01'
 
-# Fan request
-fan_dict = {
-	'language': 'EN',
-	'unit_system': 'm',
-	'username': user_ws,
-	'password': pass_ws,
-	'cmd': 'select',
-	'cmd_param': '0',
-	'zawall_mode': 'ZAWALL_PLUS',
-	'zawall_size': '4',
-	'qv': qv,
-	'psf': psf,
-	'spec_products': 'PF_00',
-	'article_no': article_no,
-	'current_phase': '3',
-	'voltage': voltage,
-	'nominal_frequency': nominal_frequency,
-	'installation_height_mm': height,
-	'installation_width_mm': width,
-	'installation_length_mm': '2000',
-	'installation_mode': 'RLT_2017',
-	'sessionid': session_id
-}
+# Loop for the code
+for i in range(len(df['Item'])):
+	for n in range(1, 7):
 
-try:
-	zawall_size = get_response(fan_dict)['ZAWALL_SIZE']
-	power_input = get_response(fan_dict)['ZA_PSYS']
+		# Set values
+		article_no = df['Item'].iloc[i]
+		gross_price = df['Gross price'].iloc[i]
 
-	print('Number of fans:', zawall_size)
-	print('Power input W:', power_input)
-	
-except:
-	print('No fan was found this time.')
+		# Fan request
+		fan_dict = {
+			'language': 'EN',
+			'unit_system': 'm',
+			'username': user_ws,
+			'password': pass_ws,
+			'cmd': 'select',
+			'cmd_param': '0',
+			'zawall_mode': 'ZAWALL_PLUS',
+			'zawall_size': n,
+			'qv': qv,
+			'psf': psf,
+			'spec_products': 'PF_00',
+			'article_no': article_no,
+			'current_phase': '3',
+			'voltage': '230',
+			'nominal_frequency': '60',
+			'installation_height_mm': height,
+			'installation_width_mm': width,
+			'installation_length_mm': '2000',
+			'installation_mode': 'RLT_2017',
+			'sessionid': session_id
+		}
+
+		try:
+			no_fans = get_response(fan_dict)['ZAWALL_SIZE']
+
+			print('Fan found:', article_no)
+			print('Number of fans:', no_fans)
+			print('\n')
+
+			total_gross = no_fans*gross_price
+
+			inner_list.append([article_no, no_fans, total_gross])
+
+			#power_input = get_response(fan_dict)['ZA_PSYS']
+			#print('Power input W:', power_input)
+			
+		except:
+			pass
+
+print(sort_function(inner_list, 2))
