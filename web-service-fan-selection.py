0d19bde6ebcf1349f33530df33e56d9063ae8e42
darroyolpz <darroyolpz@users.noreply.github.com>
2020-02-03 10:21:08 +0100

    New function of having all the fans available & more columns

--- web-service-fan-selection.py
+++ web-service-fan-selection.py
@@ -5,6 +5,9 @@ from pandas import ExcelWriter
 url = "http://fanselect.net:8079/FSWebService"
 user_ws, pass_ws = 'xxx', 'xxx'
 
+# Get all the possible fans
+all_results = True
+
 def fan_ws(request_string, url):	
 	ws_output = requests.post(url=url, data=request_string)
 	return ws_output
@@ -76,8 +79,9 @@ for j in range(len(df_data['Line'])):
 
 	# Loop for fans on each number of line
 	for i in range(len(df['Item'])):
+		max_array = 4
 		# Check several fan configuration
-		for n in range(1, 10):
+		for n in range(1, max_array):
 
 			# Set values
 			article_no = df['Item'].iloc[i]
@@ -98,8 +102,8 @@ for j in range(len(df_data['Line'])):
 				'psf': psf,
 				'spec_products': 'PF_00',
 				'article_no': article_no,
-				'current_phase': '3',
-				'voltage': '400',
+				#'current_phase': '3',
+				#'voltage': '400',
 				'nominal_frequency': '50',
 				'installation_height_mm': height,
 				'installation_width_mm': width,
@@ -116,16 +120,22 @@ for j in range(len(df_data['Line'])):
 				power_input = get_response(fan_dict)['ZA_PSYS']
 				zawall_arr = get_response(fan_dict)['ZAWALL_ARRANGEMENT']
 				no_fans = 1 if zawall_arr == 0 else int(zawall_arr[:2])
+				n_actual = get_response(fan_dict)['ERP_N_ACTUAL']
+				n_stat = get_response(fan_dict)['ERP_N_STAT']
+				n_target = get_response(fan_dict)['ERP_N_TRAGET']
+				total_gross = no_fans*gross_price
 
 				print('Number of line:', line)
 				print('Fan found:', article_no)
 				print('Power input W:', power_input)
+				print('Eff. N_actual:', n_actual)
+				print('Eff. N_stat:', n_stat)
+				print('Eff. N_target:', n_target)
 				print('Number of fans:', no_fans)
+				print('Total gross:', total_gross)
 				print('\n')
 
-				total_gross = no_fans*gross_price
-
-				inner_list.append([line, ahu, height, width, ref, qv, psf, article_no, no_fans, total_gross])
+				inner_list.append([line, ahu, height, width, ref, qv, psf, power_input, n_stat, n_target, article_no, no_fans, total_gross])
 
 				# Stop the loop
 				print('Loop stopping!')
@@ -137,14 +147,21 @@ for j in range(len(df_data['Line'])):
 
 	print("--- %s seconds ---" % (time.time() - start_time))
 	print('\n')
-	print(sort_function(inner_list, 9))
+	print(sort_function(inner_list, len(inner_list[0])-1)) # So that gross price is always the last one
 
-	# Once checked all the items and gathered the entire list, get the cheapest one
-	outter_list.append(inner_list[0])
+	# Once checked all the items and gathered the entire list, get the cheapest one only if all_results applies
+	inner_len = len(inner_list)
+
+	if all_results:
+		for row in range(inner_len):
+			outter_list.append(inner_list[row])
+	else:
+		outter_list.append(inner_list[0])
+
 	inner_list = []
 
 # Save all the results to a new dataframe
-col = ['Line', 'AHU', 'Height', 'Width', 'Ref', 'Airflow', 'Static Press.', 'article_no', 'no_fans', 'total_gross']
+col = ['Line', 'AHU', 'Height', 'Width', 'Ref', 'Airflow', 'Static Press.', 'Consump. kW', 'N_stat', 'N_target', 'Article no', 'No fans', 'Total Gross']
 result = pd.DataFrame(outter_list, columns = col)
 
 # Export to Excel
